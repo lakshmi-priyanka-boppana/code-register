176dbb63b1efe8d5a0fa854504618c3f686d719f
lakshmi priyanka boppana <lakshmi-priyanka-boppana@users.noreply.github.com>
2017-08-03 03:08:50 -0400

    Add files via upload

---
+++ pink.php
@@ -0,0 +1,17 @@
+<?php
+echo "Username <b>".$_POST['uname']."</b><br>";
+echo "welcome";
+?>
+<!DOCTYPE html>
+<html?
+<body>
+<p>
+<a href="add.html">VECTOR ADDITION</a> <br>
+<br>
+<a href="basicmatrix.html"> BASIC MULTIPLICATION MATRIX</a><br><br>
+<a href="tiledmatrix.html">TILED MATRIX</a><br><br>
+<a href="histogram.html">HISTOGRAM</a><br><br>
+<a href="reduction.html">REDUCTION</a><br><br>
+</p>
+</body>
+</html>
